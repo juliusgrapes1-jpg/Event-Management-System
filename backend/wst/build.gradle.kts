ec53c9ffdf11206a15b2a299961525ddeded283f
Mar Julius <juliusgrapes1@gmail.com>
2026-06-09 21:56:58 +0800

    Update build.gradle.kts

--- backend/wst/build.gradle.kts
+++ backend/wst/build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-r
-    runtimeOnly("com.mysql:mysql-connector-j")
+
+    runtimeOnly("org.postgresql:postgresql")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
